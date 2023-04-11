609f28895db339ccb7450e8aea8eb9ac7664f73d
nikit <nikit@DESKTOP-5363RTP>
2023-04-11 19:01:32 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+class TBook {
+public:
+	string name;
+	string author;
+	string count;
+};
+
+int main() {
+	setlocale(LC_ALL, "ru");
+
+	TBook book1;
+
+	book1.name = "I want to sleep";
+	book1.author = "PUSHKIN";
+	book1.count = "648";
+
+	cout << "\nFirst book: \n";
+	cout << "Name: " << book1.name << endl;
+	cout << "Author: " << book1.author << endl;
+	cout << "Count: " << book1.count << endl;
+
+	return 0;
+}
